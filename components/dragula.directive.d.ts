ae3dafb9d9ffd14582fa990895dfffca06c3a8d0
Jonas Holtorf <Jonas.Holtorf@4wits.de>
2016-11-19 13:39:51 +0100

    add all the js files to allow direct use

---
+++ components/dragula.directive.d.ts
@@ -0,0 +1,16 @@
+import { ElementRef, OnInit, OnChanges, SimpleChange } from '@angular/core';
+import { DragulaService } from './dragula.provider';
+export declare class DragulaDirective implements OnInit, OnChanges {
+    private el;
+    private dragulaService;
+    bag: string;
+    dragulaModel: any;
+    parent: any;
+    private container;
+    private drake;
+    constructor(el: ElementRef, dragulaService: DragulaService);
+    ngOnInit(): void;
+    ngOnChanges(changes: {
+        [propName: string]: SimpleChange;
+    }): void;
+}
